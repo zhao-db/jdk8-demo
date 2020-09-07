e1239324968ce56efb0775264d1e6a4d47cf5dfb
zhaodb <dongbao.zhao@dhc.com.cn>
2020-09-07 13:39:02 +0800

    ICBCPay “

--- src/main/java/com/example/jdk8/jdk8demo/pay/Strategry.java
+++ src/main/java/com/example/jdk8/jdk8demo/pay/Strategry.java
@@ -1,4 +1,15 @@
 package com.example.jdk8.jdk8demo.pay;
 
+import java.math.BigDecimal;
+
 public interface Strategry {
+
+    /**
+     * 计算支付金额，通过商品、渠道ID 调用各自银行银行支付
+     * @param channelId
+     * @param goodsId
+     * @return
+     */
+    BigDecimal calRecharge(Integer channelId, Integer goodsId);
+
 }
---
+++ src/main/java/com/example/jdk8/jdk8demo/pay/impl/ICBCPay.java
@@ -0,0 +1,28 @@
+package com.example.jdk8.jdk8demo.pay.impl;
+
+import com.example.jdk8.jdk8demo.pay.Strategry;
+
+import java.math.BigDecimal;
+
+/**
+ * @Description
+ * @Author zhaodb
+ * @Date 2020/9/7 11:18
+ * @Version 1.0
+ */
+public class ICBCPay implements Strategry {
+
+
+
+    /**
+     * 计算支付金额，通过商品、渠道ID 调用各自银行银行支付
+     *
+     * @param channelId
+     * @param goodsId
+     * @return
+     */
+    @Override
+    public BigDecimal calRecharge(Integer channelId, Integer goodsId) {
+        return null;
+    }
+}
