bcb99928127983026ff26e4326fb935c5498af82
zhaodb <dongbao.zhao@geekplus.com>
2023-01-09 14:22:26 +0800

    juc-init

--- src/test/java/com/example/jdk8/jdk8demo/Jdk8DemoApplicationTests.java
+++ src/test/java/com/example/jdk8/jdk8demo/Jdk8DemoApplicationTests.java
@@ -13,25 +13,5 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootTest
 class Jdk8DemoApplicationTests {
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @Test
-    void contextLoads() {
-        String body = "{\"deviceTypeId\":1,\"deviceModelId\":1,\"deviceCode\":\"2\",\"warehouseCode\":\"1\",\"deviceName\":\"光栅门2\"}";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        for (int i = 3; i < 100; i++) {
-            JSONObject js = new JSONObject();
-            js.put("deviceTypeId", 1);
-            js.put("deviceModelId", 1);
-            js.put("deviceCode", i);
-            js.put("warehouseCode", 1);
-            js.put("deviceName", "光栅门" + i);
-            HttpEntity httpEntity = new HttpEntity(js, headers);
-            String url = "http://localhost/venus/v3/device/info/add";
-            ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity(url, httpEntity, String.class);
-        }
-    }
 
 }
