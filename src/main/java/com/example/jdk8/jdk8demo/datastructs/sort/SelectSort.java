9e3a3f0acd1fad8be1ec17ef271dc6927d950ee9
zhaodb <dongbao.zhao@geekplus.com>
2023-03-31 11:44:14 +0800

    数据结构08-选择排序

--- src/main/java/com/example/jdk8/jdk8demo/datastructs/sort/SelectSort.java
+++ src/main/java/com/example/jdk8/jdk8demo/datastructs/sort/SelectSort.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
  */
 public class SelectSort {
     public static void main(String[] args) {
-        int[] arr = {5, 3, 24, 12, 34, 231, 4, 1,21312,423,42,13,41};
+        int[] arr = {5, 3, 24, 12, 34, 231, 4, 1, 21312, 423, 42, 13, 41};
         selectSort(arr);
         System.out.println("排序后结果" + Arrays.toString(arr));
 
@@ -21,9 +21,10 @@ public class SelectSort {
     public static void selectSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             int minIndex = i;
-            int minValue = array[minIndex];
-            for (int j = i; j < array.length; j++) {
-                if (minValue > array[j]) {
+            int minValue = array[i];
+            //加1的目的是假设当前I 的值就为最小 与后面比较
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < minValue) {
                     minValue = array[j];
                     minIndex = j;
                 }
