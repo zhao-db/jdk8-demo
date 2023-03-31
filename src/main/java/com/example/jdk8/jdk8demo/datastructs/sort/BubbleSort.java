ade00d9e436519e0e96f2f0cce29354c8604f79d
zhaodb <dongbao.zhao@geekplus.com>
2023-03-31 11:31:21 +0800

    数据结构08-冒泡排序

--- src/main/java/com/example/jdk8/jdk8demo/datastructs/sort/BubbleSort.java
+++ src/main/java/com/example/jdk8/jdk8demo/datastructs/sort/BubbleSort.java
@@ -1,5 +1,7 @@
 package com.example.jdk8.jdk8demo.datastructs.sort;
 
+import java.util.Arrays;
+
 /**
  * <p>
  * 冒泡排序
@@ -13,24 +15,30 @@ public class BubbleSort {
     public static void main(String[] args) {
         //测试一下冒泡排序的速度O(n^2), 给80000个数据，测试
         //创建要给80000个的随机的数组
-        int[] arr = new int[80000];
-        for (int i = 0; i < 80000; i++) {
-            arr[i] = (int) (Math.random() * 8000000); //生成一个[0, 8000000) 数
-        }
+//        int[] arr = new int[80000];
+//        for (int i = 0; i < 80000; i++) {
+//            arr[i] = (int) (Math.random() * 8000000); //生成一个[0, 8000000) 数
+//        }
+        int[] arr = {5, 3, 24, 12, 34, 231, 4, 1, 21312, 423, 42, 13, 41};
+
         long start = System.currentTimeMillis();
         //测试冒泡排序
         bubbleSort(arr);
         long end = System.currentTimeMillis();
         System.out.println("排序后的时间是=" + (end - start) / 1000 + "s");
+        System.out.println("排序后结果" + Arrays.toString(arr));
+
     }
 
 
     public static void bubbleSort(int[] array) {
+        int tmp = 0;
+        boolean flag = false;
         for (int i = 0; i < array.length - 1; i++) {
-            int tmp;
-            boolean flag = false;
+            //减i的目的是 第一次已经将最大的放到最后位置了 后续轮次不需要在和倒数第一位比较
+            //第二轮同理 所以减i
             for (int j = 0; j < array.length - 1 - i; j++) {
-                if (array[j] > array[j + 1]) {
+                if (array[j + 1] < array[j]) {
                     flag = true;
                     tmp = array[j];
                     array[j] = array[j + 1];
@@ -39,6 +47,8 @@ public class BubbleSort {
             }
             if (!flag) {
                 break;
+            } else {
+                flag = true;
             }
         }
     }
