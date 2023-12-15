c01a92b82b29a990ba5113dfc0e31609cb424768
zhaodb <dongbao.zhao@geekplus.com>
2023-12-15 18:24:14 +0800

    动态设置cron定时任务

--- src/main/java/com/example/jdk8/jdk8demo/Jdk8DemoApplication.java
+++ src/main/java/com/example/jdk8/jdk8demo/Jdk8DemoApplication.java
@@ -4,8 +4,10 @@ package com.example.jdk8.jdk8demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Jdk8DemoApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Jdk8DemoApplication.class, args);
